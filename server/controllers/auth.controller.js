2c4188de28b183e8a7664a1a1be15c339040c09b
worksahil102 <worksahil102@gmail.com>
2026-07-02 14:55:38 +0530

    Add deployed server link frontend

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const googleAuth = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
