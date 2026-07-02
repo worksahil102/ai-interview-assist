5a30ae9101c0fc5a0782473d02cbfb1374e3ae92
worksahil102 <worksahil102@gmail.com>
2026-07-02 15:04:16 +0530

    Add deployed server link frontend

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -15,8 +15,8 @@ export const googleAuth = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -28,9 +28,10 @@ export const googleAuth = async (req, res) => {
 
 export const logOut = async (req, res) => {
   try {
-    await res.clearCookie("token", {
+    res.clearCookie("token", {
       httpOnly: true,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
     });
     return res.status(200).json({ message: "LogOut Successfully" });
   } catch (error) {
